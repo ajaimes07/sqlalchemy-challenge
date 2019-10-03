6e35536486c27b42d25fceeed67755ce92333f54
Aline <ajaimes07@gmail.com>
2019-10-03 12:51:20 -0500

    Updating ClimateApp.py

--- ClimateApp.py
+++ ClimateApp.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 # 2 Database Setup
 #a. engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-engine = db.create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 #b.  reflect an existing database into a new model
 Base = automap_base()
 # c. reflect the tables
@@ -23,11 +23,8 @@ Station = Base.classes.station
 # 3. Flask Setup
 # a. import Flask
 from flask import Flask
-
 # b. Create an app, being sure to pass __name__
 app = Flask(__name__)
-
-
 # c. Define what to do when a user hits the index route
 @app.route("/")
 def Welcome():
@@ -38,7 +35,6 @@ def Welcome():
         f"/api/v1.0/stations"
         f"/api/v1.0/tobs"
     )
-        
 # d. Define what to do when a user hits the /about route
 @app.route("/api/v1.0/precipitation")
 def Precipitation():
@@ -51,24 +47,20 @@ def Precipitation():
     ppt=session.query(Measurement.prcp,Measurement.date).\
     filter(Measurement.date > qr2).\
     order_by(Measurement.date).all()
-
- # e. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+# e. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
     precipitation=[]
     for result in prcp:
         prcp_dict = {}
         prcp_dict["date"] = prcp[0]
         prcp_dict["prcp"] = prcp[1]
         precipitation.append(prcp_dict)
-    
 # f. Return the JSON representation of your dictionary.
     return jsonify(precipitation)
-
 @app.route("/api/v1.0/stations")
 def stations():
     print("Server received request for 'station' page...")
-    # Return a JSON list of stations from the dataset.
+# g.  Return a JSON list of stations from the dataset.
     return "Here is a list of the stations available"
-
     stns=session.query(Measurement).group_by(Measurement.station).count()
     print("Hello, there are {} stations available.".format(stns))
     print("Hola, existen {} estaciones disponibles.".format(stns))
@@ -80,26 +72,26 @@ def stations():
         stnsNo_dict["date"] = prcp[0]
         stnsNo_dict["prcp"] = prcp[1]
         stnsNo_total.append(stnsNo_dict)
-    #Return the JSON representation of your dictionary.
+# h. Return the JSON representation of your dictionary.
     return jsonify(stnsNo_total)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     print("Server received request for 'Precipitation' page...")
-    # query for the dates and temperature observations from a year from the last data point.
+# i. Query for the dates and temperature observations from a year from the last data point.
     return "Here is a list of the last 12 months of air temperature in Hawaii"
     TA=session.query(Measurement.tobs,Measurement.date).\
     filter(Measurement.date > qr2).\
     order_by(Measurement.date).all()
-
-    #Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+# j. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
     AT=[]
     for result in tobs:
         tobs_dict = {}
         tobs_dict["date"] = tobs[0]
         tobs_dict["tobs"] = tobs[1]
         AT.append(tobs_dict)
-    # Return a JSON list of Temperature Observations (tobs) for the previous year.
-    return jsonify(AT)
-  
-    
+# k. Return a JSON list of Temperature Observations (tobs) for the previous year.
+    return jsonify(AT) 
+
+if __name__ == '__main__':
+    app.run(debug=True)    
