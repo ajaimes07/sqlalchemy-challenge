0b3ad4502464f283fe46d344d80d6a185bdfcef4
Aline <ajaimes07@gmail.com>
2019-10-03 13:37:07 -0500

    Updating Climate App.py

--- ClimateApp.py
+++ ClimateApp.py
@@ -23,18 +23,23 @@ Station = Base.classes.station
 # 3. Flask Setup
 # a. import Flask
 from flask import Flask
+
 # b. Create an app, being sure to pass __name__
 app = Flask(__name__)
+
+
 # c. Define what to do when a user hits the index route
 @app.route("/")
 def Welcome():
     """List all available api routes."""
     return(
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
+        f"Welcome to my Hawaii Climate App!<br/>" 
+        f"Here is a list of available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
     )
+        
 # d. Define what to do when a user hits the /about route
 @app.route("/api/v1.0/precipitation")
 def Precipitation():
@@ -47,51 +52,56 @@ def Precipitation():
     ppt=session.query(Measurement.prcp,Measurement.date).\
     filter(Measurement.date > qr2).\
     order_by(Measurement.date).all()
-# e. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+
+ # e. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
     precipitation=[]
     for result in prcp:
         prcp_dict = {}
         prcp_dict["date"] = prcp[0]
         prcp_dict["prcp"] = prcp[1]
         precipitation.append(prcp_dict)
+    
 # f. Return the JSON representation of your dictionary.
     return jsonify(precipitation)
+
 @app.route("/api/v1.0/stations")
 def stations():
     print("Server received request for 'station' page...")
-# g.  Return a JSON list of stations from the dataset.
+    # Return a JSON list of stations from the dataset.
     return "Here is a list of the stations available"
-    stns=session.query(Measurement).group_by(Measurement.station).count()
-    print("Hello, there are {} stations available.".format(stns))
-    print("Hola, existen {} estaciones disponibles.".format(stns))
-    stnsNo=session.query(Measurement.station, func.count(Measurement.station).label('count')).\
-    group_by(Measurement.station).all()
-    stnsNo_total=[]
-    for result1 in stnsNo:
-        stnsNo_dict = {}
-        stnsNo_dict["date"] = prcp[0]
-        stnsNo_dict["prcp"] = prcp[1]
-        stnsNo_total.append(stnsNo_dict)
-# h. Return the JSON representation of your dictionary.
-    return jsonify(stnsNo_total)
+#    stns=session.query(Measurement).group_by(Measurement.station).count()
+#    print("Hello, there are {} stations available.".format(stns))
+#    print("Hola, existen {} estaciones disponibles.".format(stns))
+    stnsNo=session.query(Measurement.station,       func.count(Measurement.station).label('count')).\
+group_by(Measurement.station).all()
+    #stnsNo_total=[]
+#    for result1 in stnsNo:
+ #       stnsNo_dict = {}
+  #      stnsNo_dict["date"] = prcp[0]
+   #     stnsNo_dict["prcp"] = prcp[1]
+    #    stnsNo_total.append(stnsNo_dict)
+    #Return the JSON representation of your dictionary.
+    #return jsonify(stnsNo_total)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     print("Server received request for 'Precipitation' page...")
-# i. Query for the dates and temperature observations from a year from the last data point.
+    # query for the dates and temperature observations from a year from the last data point.
     return "Here is a list of the last 12 months of air temperature in Hawaii"
     TA=session.query(Measurement.tobs,Measurement.date).\
     filter(Measurement.date > qr2).\
     order_by(Measurement.date).all()
-# j. Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+
+    #Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
     AT=[]
     for result in tobs:
         tobs_dict = {}
         tobs_dict["date"] = tobs[0]
         tobs_dict["tobs"] = tobs[1]
         AT.append(tobs_dict)
-# k. Return a JSON list of Temperature Observations (tobs) for the previous year.
-    return jsonify(AT) 
-
-if __name__ == '__main__':
-    app.run(debug=True)    
+    # Return a JSON list of Temperature Observations (tobs) for the previous year.
+    return jsonify(AT)
+if __name__ == "__main__":
+    app.run(debug=True)
+  
+    
